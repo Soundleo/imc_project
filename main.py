dcda35323ee4fd6ef8a82f7f229b23ea8b82a961
Soundleo <>
2024-05-17 19:44:15 -0300

    meu primeiro commit, ngm me segura

---
+++ main.py
@@ -0,0 +1,22 @@
+print ('calculadora de IMC\n')
+
+nome = input('Informe o seu nome:\n')
+altura = str(input('Informe a sua altura\n')).replace(',','.')
+altura = float(altura)
+peso = str(input('Informe o seu peso:\n ')).replace(',','.')
+peso = float(peso)
+imc = peso/altura**2
+
+# print (f'O seu IMC é {imc}')
+
+if imc >= 35:
+    print(f'Você {nome} está comendo igual uma capivara raivosa, o seu IMC é {imc}.')
+elif imc >= 30:
+    print(f'Você {nome} está comendo igual uma capivara com fome, o seu IMC é {imc}.')
+elif imc >= 25:
+    print(f'Você {nome} está comendo igual uma capivara, o seu IMC é {imc}.')
+elif imc >= 18.5:
+    print(f'Parabéns {nome} o seu IMC é {imc}.')
+else :
+    print(f'Você {nome} está omendo igual um peixe, o seu IMC é {imc}.')
+
